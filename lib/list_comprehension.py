7db9bd40f40cb76ba0c4d7b67c1235cca12d309f
tonan <tonantruong@hotmail.com>
2023-07-23 12:10:13 -0400

    completed List Comprehensions and Generator Expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # no_evens = []
+    # evens_odds = [i if i % 2 == 0 else no_evens for i in num_list]
+    evens = [i for i in num_list if i % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    sentence = [f'{i}!' for i in sentence_list]
+    return sentence
